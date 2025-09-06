42ca7d99aff4f33c8d639b968e12a9b06d6a38b1
Sakhila2001 <154709250+Sakhila2001@users.noreply.github.com>
2025-09-06 17:20:01 +0545

    message successsfull

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FaArrowRight } from "react-icons/fa6";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const formRef = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_kf9jpip", // from EmailJS dashboard
+        "template_az4r248", // from EmailJS
+        formRef.current,
+        "YeIVJ030wWjgmDJSi" // from EmailJS account
+      )
+      .then(
+        () => {
+          alert("Message sent successfully ✅");
+        },
+        (error) => {
+          alert("Failed to send ❌: " + error.text);
+        }
+      );
+  };
+
   return (
     <div id="Contact" className="py-20">
       <div className="max-w-7xl mx-auto px-6">
@@ -12,46 +35,42 @@ const Contact = () => {
           </h1>
           <p className="mb-2">
             Have a project in mind or just want to say hi? My inbox is always
-            open. Whether you have a question, a collaboration idea, or just
-            want to connect, feel free to drop me a message. I look forward to
-            hearing from you!
+            open.
           </p>
         </div>
         <div className="max-w-3xl mx-auto">
-          <form className="space-y-6">
+          <form ref={formRef} onSubmit={sendEmail} className="space-y-6">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div>
-                <input
-                  type="text"
-                  placeholder="Enter Name"
-                  className="w-full border border-zinc-500 rounded px-4 py-3 focus:outline-none focus:ring-2 focus:ring-teal-600"
-                />
-              </div>
-              <div>
-                <input
-                  type="email"
-                  placeholder="Enter Email"
-                  className="w-full border border-zinc-500 rounded px-4 py-3 focus:outline-none focus:ring-2 focus:ring-teal-600"
-                />
-              </div>
-            </div>
-            <div>
               <input
                 type="text"
-                placeholder="Enter Subject"
-                className="w-full border border-zinc-500 rounded px-4 py-3 focus:outline-none focus:ring-2 focus:ring-teal-600"
+                name="name"
+                placeholder="Enter Name"
+                className="w-full border border-zinc-500 rounded px-4 py-3"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter Email"
+                className="w-full border border-zinc-500 rounded px-4 py-3"
               />
             </div>
-            <div>
-              <textarea
-                rows="5"
-                placeholder="Enter Message"
-                className="w-full border border-zinc-500 rounded px-4 py-3 focus:outline-none focus:ring-2 focus:ring-teal-600"
-              ></textarea>
-            </div>
+            <input
+              type="text"
+              name="subject"
+              placeholder="Enter Subject"
+              className="w-full border border-zinc-500 rounded px-4 py-3"
+            />
+            <textarea
+              name="message"
+              rows="5"
+              placeholder="Enter Message"
+              className="w-full border border-zinc-500 rounded px-4 py-3"
+            ></textarea>
             <div className="flex items-center justify-center">
-              
-              <button className="flex gap-2 items-center px-10 py-4 bg-black rounded-full text-slate-200 hover:text-white cursor-pointer">
+              <button
+                type="submit"
+                className="flex gap-2 items-center px-10 py-4 bg-black rounded-full text-slate-200 hover:text-white"
+              >
                 Send Message
                 <FaArrowRight />
               </button>
